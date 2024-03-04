7e5b087188cc7cf30cc2b6504f5498d5c3a615d3
Akshat Garg <akshatg805@gmail.com>
2024-03-04 16:43:33 +0530

    Update AppLogoName.tsx

--- app/components/NavBarComps/AppLogoName.tsx
+++ app/components/NavBarComps/AppLogoName.tsx
@@ -3,13 +3,19 @@ import Image from "next/image";
 export default function AppLogoName() {
   return (
     <>
-      <div className="flex items-center px-5">
-        <div className="flex items-center w-16">
+      <div className="flex items-center px-5 w-9/12">
+        <div className="flex items-center w-20 cursor-pointer">
           <Image src="/logo.png" alt="Logo" width={100} height={100} />
         </div>
-        <div className="flex items-center font-LogoFont pr-1 text-2xl">
+        <div className="flex items-center font-LogoFont pr-1 text-4xl cursor-pointer">
           Seat
-          <span className="text-blue-500">Sorcery</span>
+          <span
+            style={{ color: "#0048FF" }}
+            className="duration-700"
+            id="appLogoNameSpan"
+          >
+            Sorcery
+          </span>
         </div>
       </div>
     </>
