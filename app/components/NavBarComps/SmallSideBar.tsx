a9695cf1208a1ff3c3bf64920221456259a0f21d
Akshat Garg <akshatg805@gmail.com>
2024-03-07 21:03:52 +0530

    Update SmallSideBar.tsx

--- app/components/NavBarComps/SmallSideBar.tsx
+++ app/components/NavBarComps/SmallSideBar.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Link from "next/link";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
-import { motion, AnimatePresence } from "framer-motion";
+import SettingsIcon from "@mui/icons-material/Settings";
+import HomeIcon from "@mui/icons-material/Home";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import LogInButton from "./LogInButton";
 
 export default function SmallSideBar() {
   const [navOpen, setNavOpen] = useState(false);
@@ -38,7 +43,32 @@ export default function SmallSideBar() {
               type: "tween",
               duration: "0.7",
             }}
-          ></motion.div>
+          >
+            <div className="h-28 w-full border-b-4 mb-10" />
+            <Link href={"/"} className="text-xl ml-5">
+              <HomeIcon sx={{ fontSize: "20px", marginBottom: "4px" }} /> {"  "}
+              Home
+              <div className="w-full h-1 bg-blue-800 mb-10" />
+            </Link>
+            <Link href={"/services"} className="text-xl ml-5">
+              <SettingsIcon sx={{ fontSize: "20px", marginBottom: "4px" }} />{" "}
+              {"  "}
+              Services
+              <div className="w-full h-1 bg-blue-800 mb-10" />
+            </Link>
+            <Link href={"/support"} className="text-xl ml-5">
+              <SupportAgentIcon
+                sx={{ fontSize: "20px", marginBottom: "4px" }}
+              />{" "}
+              {"  "}Support
+              <div className="w-full h-1 bg-blue-800 mb-10" />
+            </Link>
+            <div className="w-full flex justify-center">
+              <Link href={"/login"}>
+                <LogInButton />
+              </Link>
+            </div>
+          </motion.div>
         )}
       </AnimatePresence>
     </>
