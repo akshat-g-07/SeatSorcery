081045676b9eefe8206095b2e152c6229b25c4c8
Akshat Garg <akshatg805@gmail.com>
2024-03-08 21:11:02 +0530

    Update NavSideBar.tsx

--- app/components/NavBarComps/NavSideBar.tsx
+++ app/components/NavBarComps/NavSideBar.tsx
@@ -2,17 +2,18 @@
 
 import { useState, useEffect } from "react";
 import SmallSideBar from "./SmallSideBar";
+import LargeSideBar from "./LargeSideBar";
 
 export default function NavSideBar() {
   const [screenSize, setScreenSize] = useState("small");
 
   const handleResize = () => {
-    let tempScreenSize = window.innerWidth > 425 ? "large" : "small";
+    let tempScreenSize = window.innerWidth > 768 ? "large" : "small";
     setScreenSize(tempScreenSize);
   };
 
   useEffect(() => {
-    let tempScreenSize = window.innerWidth > 425 ? "large" : "small";
+    let tempScreenSize = window.innerWidth > 768 ? "large" : "small";
     setScreenSize(tempScreenSize);
     window.addEventListener("resize", handleResize);
     return () => {
@@ -21,8 +22,12 @@ export default function NavSideBar() {
   }, []);
 
   return (
-    <div className="h-full w-3/12">
-      {screenSize === "small" ? <SmallSideBar /> : "LargeSideBar"}
+    <div className="h-full w-3/12 md:w-8/12">
+      {screenSize === "small" && window.innerWidth < 768 ? (
+        <SmallSideBar />
+      ) : (
+        <LargeSideBar />
+      )}
     </div>
   );
 }
