a12a7e822f2938257e76951a59fc27bfeb16b7c2
Akshat Garg <akshatg805@gmail.com>
2024-03-10 22:44:30 +0530

    25 NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,7 +4,7 @@ import NavSideBar from "./NavSideBar";
 export default function NavBar() {
   return (
     <>
-      <nav className="w-screen h-24 fixed z-50 top-0 left-0 flex">
+      <nav className="w-screen h-24 fixed z-50 top-0 left-0 flex bg-transparent">
         <AppLogoName />
         <NavSideBar />
       </nav>
