77f0e82258115763f1d0cc8d4aaa2fd3a3bb6316
Akshat Garg <akshatg805@gmail.com>
2024-03-11 23:23:05 +0530

    Update page.tsx

--- app/services/TrainSchedule/page.tsx
+++ app/services/TrainSchedule/page.tsx
@@ -33,7 +33,7 @@ export default function TrainSchedulePage() {
       const apiResponseVal = await apiResponse.json();
 
       apiResponseVal.status
-        ? setResponseError(false)
+        ? setResponseError(true)
         : setResponse(apiResponseVal);
       setLoading(false);
     }
