89f548dad8287510742436e31381a08881b3e34d
Akshat Garg <akshatg805@gmail.com>
2024-03-14 21:03:26 +0530

    30 AppLogoName.tsx

--- app/components/NavBarComps/AppLogoName.tsx
+++ app/components/NavBarComps/AppLogoName.tsx
@@ -7,7 +7,13 @@ export default function AppLogoName() {
       <div className="flex items-center px-5 w-9/12 md:w-4/12">
         <Link href={"/"} className="flex">
           <div className="flex items-center w-20 cursor-pointer">
-            <Image src="/logo.png" alt="Logo" width={100} height={100} />
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={100}
+              height={100}
+              priority={true}
+            />
           </div>
           <div className="flex items-center font-LogoFont pr-1 text-4xl cursor-pointer">
             Seat
