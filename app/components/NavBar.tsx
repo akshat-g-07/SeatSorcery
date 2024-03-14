abd19b13c6de00d32d075437ddf1d27b407be246
Akshat Garg <akshatg805@gmail.com>
2024-03-14 21:02:51 +0530

    29 NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,10 +1,10 @@
-import AppLogoName from "./AppLogoName";
-import NavSideBar from "./NavSideBar";
+import AppLogoName from "./NavBarComps/AppLogoName";
+import NavSideBar from "./NavBarComps/NavSideBar";
 
 export default function NavBar() {
   return (
     <>
-      <nav className="w-screen h-24 sticky z-50 top-0 left-0 flex bg-transparent">
+      <nav className="w-screen h-24 sticky z-50 top-0 left-0 flex bg-[#F0F8FF]">
         <AppLogoName />
         <NavSideBar />
       </nav>
