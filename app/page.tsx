8c49a1b3464d25b68e8a624735cccb380334faa8
Akshat Garg <akshatg805@gmail.com>
2024-03-16 22:25:13 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import IntroSection from "./components/HomePageComps/IntroSection";
+import ServicesSection from "./components/HomePageComps/ServicesSection";
 
 export default function Home() {
   return (
     <>
       <IntroSection />
+      <ServicesSection />
     </>
   );
 }
