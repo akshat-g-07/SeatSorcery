207a13f4bb978dde35bdaefa68c6e5f80aa8cb8d
Akshat Garg <akshatg805@gmail.com>
2024-03-19 19:19:02 +0530

    Create page.tsx

---
+++ seatsorcery/app/page.tsx
@@ -0,0 +1,13 @@
+import FAQSection from "./components/HomePageComps/FAQSection";
+import IntroSection from "./components/HomePageComps/IntroSection";
+import ServicesSection from "./components/HomePageComps/ServicesSection";
+
+export default function Home() {
+  return (
+    <>
+      <IntroSection />
+      <ServicesSection />
+      <FAQSection />
+    </>
+  );
+}
