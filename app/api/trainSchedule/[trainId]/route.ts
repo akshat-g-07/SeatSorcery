a22fac2fc123b4b3d708bb03800fbdda22f3bc51
Akshat Garg <akshatg805@gmail.com>
2024-04-28 23:07:37 +0530

    78 route.ts

--- app/api/trainSchedule/[trainId]/route.ts
+++ app/api/trainSchedule/[trainId]/route.ts
@@ -1,23 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+
 interface GetContext {
   params: { trainId: string };
 }
 
-export async function GET(request: Request, context: GetContext) {
+export async function GET(request: NextRequest, context: GetContext) {
   try {
     const response = await fetch(
       "https://api.railwayapi.site/api/v1/trains/" + context.params.trainId
     );
     const responseVal = await response.json();
+
+    if (responseVal.errors)
+      return new NextResponse(
+        JSON.stringify({ errorMessage: "Train Not Found" }),
+        {
+          status: 404,
+        }
+      );
+
     const responseData = responseVal.data[0];
     const { trainNumber, trainName, trainFullName, trainRunsOn, ...restProps } =
       responseData;
-    return Response.json({
-      trainNumber,
-      trainName,
-      trainFullName,
-      trainRunsOn,
-    });
+
+    return new NextResponse(
+      JSON.stringify({
+        trainNumber,
+        trainName,
+        trainFullName,
+        trainRunsOn,
+      }),
+      { status: 200 }
+    );
   } catch (error) {
-    return Response.json({ status: 404 });
+    console.error(error);
+    return new NextResponse(
+      JSON.stringify({ errorMessage: "Something on API side went wrong" }),
+      { status: 500 }
+    );
   }
 }
