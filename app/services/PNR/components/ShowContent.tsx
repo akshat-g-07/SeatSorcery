76d1bc60f868d84e7b2fd4d654865cddf57e32e5
Akshat Garg <akshatg805@gmail.com>
2024-05-27 23:17:45 +0530

    118 ShowContent.tsx

--- app/services/PNR/components/ShowContent.tsx
+++ app/services/PNR/components/ShowContent.tsx
@@ -5,14 +5,18 @@ export default function ShowContent({
 }: {
   responseData: PNRDetails | null;
 }) {
-  const Date01 =
-    responseData?.arrival_data.arrival_date &&
-    new Date(responseData.arrival_data.arrival_date);
-  const Date02 =
-    responseData?.departure_data.departure_date &&
-    new Date(responseData.departure_data.departure_date);
+  const boardingTime =
+    responseData?.data.boardingInfo.arrivalTime &&
+    new Date("1970-01-01T" + responseData.data.boardingInfo.arrivalTime + "Z");
+  const destinationTime =
+    responseData?.data.destinationInfo.arrivalTime &&
+    new Date(
+      "1970-01-01T" + responseData.data.destinationInfo.arrivalTime + "Z"
+    );
   const diffInMs =
-    Date01 && Date02 && Math.abs(Date01.getTime() - Date02.getTime());
+    boardingTime &&
+    destinationTime &&
+    Math.abs(boardingTime.getTime() - destinationTime.getTime());
   const hours = diffInMs && Math.floor(diffInMs / 3600000);
   const minutes = diffInMs && Math.floor((diffInMs % 3600000) / 60000);
 
@@ -21,57 +25,82 @@ export default function ShowContent({
       <div className="w-full h-full font-semibold p-10 flex flex-col">
         <div className="w-full h-max flex items-end justify-center">
           <span className="border-r-2 border-l-2 px-2 border-black h-full items-end flex text-xs w-1/12">
-            {responseData?.train_name.split("-")[1]}
+            {responseData?.data.trainInfo.trainNo}
           </span>
           <span className="border-r-2 px-2 border-black h-full items-end flex text-3xl font-bold w-6/12">
-            {responseData?.train_name.split("-")[0]}
+            {responseData?.data.trainInfo.name}
           </span>
         </div>
         <div className="w-full h-max flex mt-6">
           <div className="w-1/2 h-24 flex flex-col justify-center">
             <p className="text-2xl font-bold">
-              {responseData?.boarding_station}
+              <span className="text-xs text-black/70 mr-2">
+                {responseData?.data.boardingInfo.stationCode}
+              </span>
+              {responseData?.data.boardingInfo.stationName}
             </p>
-            <p className="font-normal mt-4">
-              {responseData?.departure_data.departure_date}
+            <p className="font-semibold text-lg">
+              {responseData?.data.trainInfo.dt}&nbsp;
+              <span className="text-sm text-black/50">
+                +{Number(responseData?.data.boardingInfo.travellingDay) - 1}
+              </span>
+            </p>
+            <p className="font-normal text-sm">
+              {responseData?.data.boardingInfo.arrivalTime}
             </p>
           </div>
           <div className="w-[2px] h-24 flex flex-col items-center">
             <div className="w-full h-6 bg-black" />
-            <div className="w-24 h-12 bg-slate-300/50 rounded-md sticky flex flex-col items-center justify-center">
+            <div className="w-max px-4 h-12 bg-slate-300/50 rounded-md sticky flex flex-col items-center justify-center">
               <p>
                 {hours}:{minutes}
               </p>
-              <p>{responseData?.class}</p>
+              <p>
+                <span className="text-sm text-black/75">Journey Day(s):</span>
+                {Number(responseData?.data.destinationInfo.travellingDay) -
+                  Number(responseData?.data.boardingInfo.travellingDay) +
+                  1}
+              </p>
             </div>
             <div className="w-full h-6 bg-black" />
           </div>
           <div className="w-1/2 h-24 flex flex-col justify-center text-right">
             <p className="text-2xl font-bold">
-              {responseData?.reservation_upto}
+              {responseData?.data.destinationInfo.stationName}
+              <span className="text-xs text-black/70 ml-2">
+                {responseData?.data.destinationInfo.stationCode}
+              </span>
             </p>
-            <p className="font-normal mt-4">
-              {responseData?.arrival_data.arrival_date}
+            <p className="font-semibold text-lg">
+              {responseData?.data.trainInfo.dt}&nbsp;
+              <span className="text-sm text-black/50">
+                +{Number(responseData?.data.destinationInfo.travellingDay) - 1}
+              </span>
+            </p>
+            <p className="font-normal text-sm">
+              {responseData?.data.destinationInfo.arrivalTime}
             </p>
           </div>
         </div>
-        <div className="w-full h-max flex mt-5 flex-wrap gap-x-5 justify-between">
-          {responseData?.passenger.map((item) => {
-            return (
-              <div
-                key={item.name}
-                className="w-[45%] m-4 p-4 border-r-2 border-black"
-              >
-                <p className="text-xl font-semibold">Name: {item.name}</p>
-                <p className="text-sm text-black/75">
-                  Booking Status: {item.booking_status}
-                </p>
-                <p className="text-sm text-black/75">
-                  Current Status: {item.current_status}
-                </p>
-              </div>
-            );
-          })}
+        <div className="w-full h-max mt-5">
+          <p>Seat(s):</p>
+          <div className="w-full flex flex-wrap gap-x-5 justify-between">
+            {responseData?.data.passengerInfo.map((item) => {
+              return (
+                <div
+                  key={item.currentBerthNo}
+                  className="w-[45%] m-4 p-4 border-r-2 border-black"
+                >
+                  <p className="text-sm text-black/75">
+                    Booking Status: {item.currentCoach}
+                  </p>
+                  <p className="text-sm text-black/75">
+                    Current Status: {item.currentBerthNo}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
