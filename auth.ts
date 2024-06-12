2e2fa261921b6c00f61112b40c248345d823b246
Akshat Garg <akshatg805@gmail.com>
2024-06-12 23:47:46 +0530

    140 auth.ts

--- auth.ts
+++ auth.ts
@@ -1,15 +1,15 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import authConfig from "./auth.config";
-
-const prisma = new PrismaClient();
+import { db } from "./lib/db";
 
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut,
 } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
 });
