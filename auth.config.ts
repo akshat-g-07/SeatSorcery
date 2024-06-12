cba470d54c2623acfef1922275f0f3d964e4410f
Akshat Garg <akshatg805@gmail.com>
2024-06-12 23:47:20 +0530

    139 auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,16 +1,37 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcryptjs";
 
 export default {
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     Credentials({
-      async authorize(credentials) {
-        console.log(credentials);
+      async authorize(credentials, request) {
+        const { email, password } = credentials;
+
+        if (!email) {
+          return null;
+        }
+
+        if (!password) {
+          return null;
+        }
+        const user = await getUserByEmail(email);
+        if (!user || !user.password) {
+          return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+          return user;
+        }
+
         return null;
       },
     }),
