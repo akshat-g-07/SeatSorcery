a1e3d61b08985186a2bcd6f8ea4013328ed9b129
Akshat Garg <akshatg805@gmail.com>
2024-06-16 23:42:40 +0530

    148 CardWrapper.tsx

--- app/auth/components/CardWrapper.tsx
+++ app/auth/components/CardWrapper.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import GoogleIcon from "@mui/icons-material/Google";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,6 +12,8 @@ import {
 } from "@/components/ui/card";
 import { ReactNode } from "react";
 import Link from "next/link";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { signIn } from "next-auth/react";
 
 export default function CardWrapper({
   cardTitle,
@@ -27,8 +31,8 @@ export default function CardWrapper({
   footerButtonLabel: string;
 }) {
   return (
-    <div className="w-full h-auto min-h-[calc(100vh-6rem)] flex flex-col items-center justify-center">
-      <Card className="shadow-lg w-80">
+    <div className="w-full h-auto my-10 min-h-[calc(100vh-6rem)] flex flex-col items-center justify-center">
+      <Card className="shadow-lg w-fit min-w-80">
         <CardHeader className="text-center">
           <CardTitle className="font-semibold text-xl">
             {cardTitle.toUpperCase()}
@@ -36,19 +40,26 @@ export default function CardWrapper({
           <CardDescription>{cardDescription}</CardDescription>
         </CardHeader>
         <CardContent>{children}</CardContent>
-        <CardFooter className="flex-col">
-          {showSocial && (
-            <>
-              <Button variant="outline">
-                <GoogleIcon />
-                &nbsp;&nbsp; {cardTitle} With Google
-              </Button>
-            </>
-          )}
-          <Button variant="link" className="mt-2">
-            <Link href={footerButtonLink}>{footerButtonLabel}</Link>
-          </Button>
-        </CardFooter>
+        {footerButtonLabel && footerButtonLink && (
+          <CardFooter className="flex-col">
+            {showSocial && (
+              <>
+                <Button
+                  variant="outline"
+                  onClick={() => {
+                    signIn("google", { callbackUrl: DEFAULT_LOGIN_REDIRECT });
+                  }}
+                >
+                  <GoogleIcon />
+                  &nbsp; Continue With Google
+                </Button>
+              </>
+            )}
+            <Button variant="link" className="mt-2">
+              <Link href={footerButtonLink}>{footerButtonLabel}</Link>
+            </Button>
+          </CardFooter>
+        )}
       </Card>
     </div>
   );
