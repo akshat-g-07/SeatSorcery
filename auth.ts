1059bac68b527b0216c083d821d1dc79e5549cbe
Akshat Garg <akshatg805@gmail.com>
2024-07-03 23:19:19 +0530

    157 auth.ts

--- auth.ts
+++ auth.ts
@@ -9,6 +9,17 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
