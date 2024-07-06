9e8807a625c276a4d7aed156169304ed4a696a12
Akshat Garg <akshatg805@gmail.com>
2024-07-07 00:13:08 +0530

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -40,6 +40,13 @@ export interface ButtonProps
   asChild?: boolean
 }
 
+  
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
