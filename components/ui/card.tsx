d3c62ee51a9b6b5ec9a325e271ff9166c8176c1e
Akshat Garg <akshatg805@gmail.com>
2024-07-08 19:17:42 +0530

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -18,6 +18,17 @@ const Card = React.forwardRef<
 Card.displayName = "Card"
 
 
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    {...props}
+  />
+))
+CardHeader.displayName = "CardHeader"
 
 const CardHeader = React.forwardRef<
   HTMLDivElement,
@@ -30,6 +41,7 @@ const CardHeader = React.forwardRef<
   />
 ))
 CardHeader.displayName = "CardHeader"
+
 
 const CardTitle = React.forwardRef<
   HTMLParagraphElement,
