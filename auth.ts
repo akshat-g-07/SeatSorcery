86acdf9ef9e73b90231ed9fe8438d5f121854616
Akshat Garg <akshatg805@gmail.com>
2024-07-13 23:41:49 +0530

    173 auth.ts

--- auth.ts
+++ auth.ts
@@ -1,26 +1,6 @@
-import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import authConfig from "./auth.config";
-import { db } from "./lib/db";
+import { auth } from "@/auth";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  pages: {
-    signIn: "/auth/login",
-  },
-  events: {
-    async linkAccount({ user }) {
-      await db.user.update({
-        where: { id: user.id },
-        data: { emailVerified: new Date() },
-      });
-    },
-  },
-  adapter: PrismaAdapter(db),
-  session: { strategy: "jwt" },
-  ...authConfig,
-});
+export const currentUser = async () => {
+  const session = await auth();
+  return session?.user;
+};
